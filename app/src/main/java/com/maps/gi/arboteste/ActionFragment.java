9415c2230a55dd5bf5500cd2a0781b619709c567
ccounirp <ccounirp@users.noreply.github.com>
2018-09-15 16:55:54 -0300

    Initial commit

---
+++ app/src/main/java/com/maps/gi/arboteste/ActionFragment.java
@@ -0,0 +1,32 @@
+package com.maps.gi.arboteste;
+
+
+import android.content.Intent;
+import android.graphics.Camera;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ActionFragment extends Fragment {
+
+
+    public ActionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_action, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/maps/gi/arboteste/MainActivity.java
@@ -0,0 +1,33 @@
+package com.maps.gi.arboteste;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bCamera = (Button)findViewById(R.id.btnCamera);
+
+        bCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirCamera();
+            }
+        });
+    }
+
+    public final void abrirCamera(){
+
+        Intent camera = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(camera);
+
+    }
+
+}
