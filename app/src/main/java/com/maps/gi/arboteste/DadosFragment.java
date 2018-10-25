8bc7f70958170fbae8b7c6b73e95b4e477f7cc5e
ccounirp <ccounirp@users.noreply.github.com>
2018-10-25 15:39:20 -0300

    Excluido o ActionFragment, foi criado um DadosFragment, onde contém as informações que vão ter que ser salvas, junto com a imagem e o ponto no mapa. Alterado o layout apenas pra melhor visualização de como seria o sistema.

--- app/src/main/java/com/maps/gi/arboteste/DadosFragment.java
+++ app/src/main/java/com/maps/gi/arboteste/DadosFragment.java
@@ -14,10 +14,10 @@ import android.widget.Button;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ActionFragment extends Fragment {
+public class DadosFragment extends Fragment {
 
 
-    public ActionFragment() {
+    public DadosFragment() {
         // Required empty public constructor
     }
 
@@ -26,7 +26,7 @@ public class ActionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_action, container, false);
+        return inflater.inflate(R.layout.fragment_dados, container, false);
     }
 
 }
--- app/src/main/java/com/maps/gi/arboteste/MainActivity.java
+++ app/src/main/java/com/maps/gi/arboteste/MainActivity.java
@@ -3,6 +3,7 @@ package com.maps.gi.arboteste;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -34,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
                 abrirMapa();
             }
         });
+
+
 
     }
 
