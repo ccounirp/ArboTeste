27212f77a52ccf333775be71b4253cfd114fb55f
Bruno <ccounirp2018@outlook.com>
2018-10-29 15:17:33 -0300

    Método para conexão com SQL SERVER

--- app/src/main/java/com/maps/gi/arboteste/MainActivity.java
+++ app/src/main/java/com/maps/gi/arboteste/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Location;
 import android.os.Debug;
+import android.os.StrictMode;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -25,11 +28,27 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView foto;
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationClient;
+    private TextView txtNomePopular;
+    private TextView txtNomeCientifico;
+    private TextView txtCpa;
+    private TextView txtAlturaTronco;
+    private TextView txtAlturaCopa;
+    private TextView txtNotaRaiz;
+    private TextView txtNotaCaule;
+    private TextView txtVigorCopa;
+    private TextView txtNotaVitArv;
+    private TextView txtDoencasPragPara;
+    private TextView txtObservacoes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         foto = findViewById(R.id.imgArvore);
         final EditText latitude = findViewById(R.id.idNomePopular);
 
+        associaComponentes();
 
         requestPermissions();
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
@@ -62,6 +82,9 @@ public class MainActivity extends AppCompatActivity {
         bCriar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                gravarRegistro();
+
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     return;
                 }
@@ -78,6 +101,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    public Connection conexionCB(){
+        Connection conexion=null;
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            conexion = DriverManager.getConnection("jdbc:jtds:sqlserver://189.68.139.34;databaseName=Arbo;user=sa;password=epilef;");
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+        return conexion;
+    }
+
+    public void gravarRegistro(){
+        try {
+            PreparedStatement pst = conexionCB().prepareStatement("INSERT INTO ARVORE_REGISTRO" +
+                    "(NOME_POPULAR,NOME_CIENTIFICO,CPA,ALTURA_TRONCO,ALTURA_COPA,NOTA_RAIZ,NOTA_CAULE," +
+                    "VIGOR_COPA,NOTA_VITALIDADE,NOTA_DOENCAS,OBS) " +
+                    "VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+
+            pst.setString(1,txtNomePopular.getText().toString());
+            pst.setString(2,txtNomeCientifico.getText().toString());
+            pst.setString(3,txtCpa.getText().toString());
+            pst.setString(4,txtAlturaTronco.getText().toString());
+            pst.setString(5,txtAlturaCopa.getText().toString());
+            pst.setString(6,txtNotaRaiz.getText().toString());
+            pst.setString(7,txtNotaCaule.getText().toString());
+            pst.setString(8,txtVigorCopa.getText().toString());
+            pst.setString(9,txtNotaVitArv.getText().toString());
+            pst.setString(10,txtDoencasPragPara.getText().toString());
+            pst.setString(11,txtObservacoes.getText().toString());
+
+            pst.executeUpdate();
+
+            Toast.makeText(getApplicationContext(),"Registro gravado com sucesso!",Toast.LENGTH_LONG).show();
+
+        }catch (SQLException e){
+            Toast.makeText(getApplicationContext(),"Ocorreu um erro ao gravar o registro: " + e.toString(),Toast.LENGTH_LONG).show();
+        }
     }
 
     public final void abrirCamera(){
@@ -104,5 +169,20 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1
         );
+    }
+
+    public void associaComponentes(){
+
+        txtNomePopular=(TextView) findViewById(R.id.idNomePopular);
+        txtNomeCientifico=(TextView) findViewById(R.id.idNomeCientifico);
+        txtCpa=(TextView) findViewById(R.id.idCpa);
+        txtAlturaTronco=(TextView) findViewById(R.id.idAlturaTronco);
+        txtAlturaCopa=(TextView) findViewById(R.id.idAlturaCopa);
+        txtNotaRaiz=(TextView) findViewById(R.id.idNotaRaiz);
+        txtNotaCaule=(TextView) findViewById(R.id.idNotaCaule);
+        txtVigorCopa=(TextView) findViewById(R.id.idVigorCopa);
+        txtNotaVitArv=(TextView) findViewById(R.id.idNotaVitArv);
+        txtDoencasPragPara=(TextView) findViewById(R.id.idDoencasPragPara);
+        txtObservacoes=(TextView) findViewById(R.id.idObservacoes);
     }
 }
