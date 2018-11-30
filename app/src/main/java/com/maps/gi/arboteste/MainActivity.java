5d6420b7c071e89aeb5ca6998fa9d2844d18a33a
ccounirp <ccounirp@users.noreply.github.com>
2018-11-30 15:01:08 -0200

    Mudança na tela

--- app/src/main/java/com/maps/gi/arboteste/MainActivity.java
+++ app/src/main/java/com/maps/gi/arboteste/MainActivity.java
@@ -79,8 +79,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+          //      WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         ImageButton bCamera = findViewById(R.id.btnCamera);
         ImageButton bMapa = findViewById(R.id.btnMapa);
